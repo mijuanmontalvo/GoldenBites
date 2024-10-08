c7887fe937246ddefb7c7def40ac5c264f6a2a44
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2024-10-08 15:44:51 -0700

    Test about router file

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php 
 
-function dd($value){
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-    die();
-}
+
 
 function urlIs($value){
     return $_SERVER['REQUEST_URI']===$value;
