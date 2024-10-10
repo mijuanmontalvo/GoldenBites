2a32bdb3e158f4b9c03477f9770351ad0ca0df7e
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2024-10-10 15:59:57 -0700

    Create initial page of reservations

---
+++ Views/reservation.view.php
@@ -0,0 +1,29 @@
+
+
+  <!-- header / hero -->
+  <?php require('partials/head.php')?> 
+
+  <!-- navigation -->
+  <?php require('partials/nav.php')?> 
+
+
+  <ul class="">
+      <li>
+        
+        <h3>Make a reservation</h3>
+        <p>Here you can make reservations for your favorite meals within our kitchen hours.</p>
+        <a href="makereservation">Continue...<i class=""></i></a>
+      </li>
+      <li>
+        
+        <h3>Check your reservations</h3>
+        <p>Here you can review all the reservations you have with us, your order, date and time are included here.</p>
+        <a href="reservationsmade">Continue...<i class=""></i></a>
+      </li>
+
+    </ul>
+
+  <!-- Footer -->
+
+
+<?php require('partials/footer.php')?>
