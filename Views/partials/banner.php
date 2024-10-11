fe581f81914d0b67962216aa02fbc42ff90f2767
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2024-10-11 12:52:27 -0700

    Create general banner of pages

---
+++ Views/partials/banner.php
@@ -0,0 +1,13 @@
+<section class="trips" id="trips">
+    <article>
+      
+      <h2>Welcome <?php echo $_SESSION['username'] ?> to </h2>
+    </article>
+    <article>
+   
+      <h1><?= $heading?></h1>
+      <p><?= $heading2?></p>
+      
+    </article>
+
+  </section>
