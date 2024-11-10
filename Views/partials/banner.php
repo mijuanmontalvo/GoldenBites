5c9de60869898f6e42350604c367a11f2be866ec
mijuanmontalvo <mijuanmontalvo@yahoo.com>
2024-11-10 13:16:11 -0800

    Improve css of app

--- Views/partials/banner.php
+++ Views/partials/banner.php
@@ -1,7 +1,7 @@
 <section class="trips" id="trips">
     <article>
       
-      <p class="welcome">Welcome <?php echo $_SESSION['username'] ?> </p>
+      <p class="welcome">Welcome <?php echo $_SESSION['Name']?> (<?php echo $_SESSION['username'] ?> - <?php echo $_SESSION['UserType'] ?>) </p>
     </article>
     <article>
    
